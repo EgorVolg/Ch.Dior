ece71d13d63e8c6f2ef8808f92a84917917e25b6
Valery <duna9588@gmail.com>
2024-06-09 21:47:30 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const swiperTop = new Swiper(".top-slider", {
+  effect: "fade",
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
